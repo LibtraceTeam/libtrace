c7925900a6b7340c0e48e15f8b890a2b4ef974c2
Daniel Lawson <dlawson@cs.waikato.ac.nz>
2004-06-22 05:22:54 +0000

    accepts a filter on the cmdline

--- examples/protocol/protocol.c
+++ examples/protocol/protocol.c
@@ -14,6 +14,7 @@ char *buffer[4096];
 int main(int argc, char *argv[]) {
 
         char *hostname = "rtclient:chasm.cs.waikato.ac.nz";
+	char *filterstring = 0;
 	struct libtrace_ip *ipptr = 0;
 	
         int status; // need to pass to rtclient_read_packet
@@ -21,15 +22,25 @@ int main(int argc, char *argv[]) {
         if (argc == 2) {
                 hostname = argv[1];
         }
+	if (argc == 3) {
+		hostname = argv[1];
+		filterstring = argv[2];
+	}
 
         // create an rtclient to hostname, on the default port
         trace = create_trace(hostname);
+	if (filterstring) {
+		libtrace_bpf_setfilter(trace,filterstring);
+	}
 
         for (;;) {
                 if ((psize = libtrace_read_packet(trace, buffer,4096, &status)) <= 0) {
                         // terminate
                         break;
                 }
+		if (!libtrace_bpf_filter(trace, buffer, 4096)) {
+			continue;
+		}
 	 	ipptr = get_ip(trace,buffer,4096);
 		if (ipptr) {
 			printf("%d:%d\n",ipptr->ip_p,get_link_type(trace,buffer,4096));
