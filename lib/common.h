6ad8afc1602612c6b8b213e6c3fe3e95dea22057
Daniel Lawson <dlawson@cs.waikato.ac.nz>
2004-08-10 00:55:56 +0000

    adding common.h

---
+++ lib/common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H 1
+
+#ifdef __cplusplus
+#  define BEGIN_C_DECLS extern "C" {
+#  define END_C_DECLS   }
+#else /* !__cplusplus */
+#  define BEGIN_C_DECLS
+#  define END_C_DECLS
+#endif /* __cplusplus */
+
+#endif // COMMON_H
