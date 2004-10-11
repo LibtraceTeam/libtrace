886c3d9aa9e2efd4ac93ecc03024f25a8e3e5ed1
Daniel Lawson <dlawson@cs.waikato.ac.nz>
2004-10-11 01:30:08 +0000

    changing version to 2.1.0

--- config.h
+++ config.h
@@ -68,13 +68,13 @@
 #define PACKAGE_NAME "libtrace"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libtrace 2.0.6"
+#define PACKAGE_STRING "libtrace 2.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libtrace"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.6"
+#define PACKAGE_VERSION "2.1.0"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -83,7 +83,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "2.0.6"
+#define VERSION "2.1.0"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
