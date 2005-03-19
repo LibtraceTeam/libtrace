53991faaef28414cf888bb1c12f43f96f1e07ef3
Daniel Lawson <dlawson@cs.waikato.ac.nz>
2005-03-19 03:45:33 +0000

    updated changelog, removed an extra , from an enum

--- lib/libtrace.h
+++ lib/libtrace.h
@@ -374,7 +374,7 @@ typedef enum {
        TRACE_TYPE_HDLC_POS, 
        TRACE_TYPE_ETH,
        TRACE_TYPE_ATM,
-       TRACE_TYPE_80211,
+       TRACE_TYPE_80211
      } libtrace_linktype_t;
 
 /** Get the type of the link layer
