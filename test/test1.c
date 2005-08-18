06b2bcf57ba71163a912abf4fcecec66e75e5f9e
Daniel Lawson <dlawson@cs.waikato.ac.nz>
2005-08-18 04:30:45 +0000

    adding makefile and some test programs

---
+++ test/test1.c
@@ -0,0 +1,65 @@
+/*
+ * This file is part of libtrace
+ *
+ * Copyright (c) 2004 The University of Waikato, Hamilton, New Zealand.
+ * Authors: Daniel Lawson 
+ *          Perry Lorier 
+ *          
+ * All rights reserved.
+ *
+ * This code has been developed by the University of Waikato WAND 
+ * research group. For further information please see http://www.wand.net.nz/
+ *
+ * libtrace is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libtrace is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libtrace; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "dagformat.h"
+#include "libtrace.h"
+
+
+int main(int argc, char *argv[]) {
+        char *uri = "erf:./traces/100_packets.erf";
+	struct libtrace_t *trace;
+	
+	trace = trace_create(uri);
+	
+	if (trace) {
+		printf("success: can open erf traces\n");	
+		trace_destroy(trace);
+		return 0;
+	}
+	printf("failure: cannot open erf traces\n");
+	trace_perror("trace_create");
+	return -1;
+}
