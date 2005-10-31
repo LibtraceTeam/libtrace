185162460d0cf2405e689c9fa90ade2c40c9d726
Perry Lorier <perry@waikato.ac.nz>
2005-10-31 02:47:37 +0000

    Make dest/source obvious

--- libpacketdump/link_2.cc
+++ libpacketdump/link_2.cc
@@ -11,20 +11,20 @@ void decode(int link_type,char *packet,int len)
 {
 	printf(" Ethernet:");
 	if (len>=6)
-		printf(" %s",ether_ntoa((struct ether_addr*)packet));
+		printf(" Dest: %s",ether_ntoa((struct ether_addr*)packet));
 	else {
 		printf("[|Truncated]\n");
 		return;
 	}
 	if (len>=12) 
-		printf(" %s",ether_ntoa((struct ether_addr*)(packet+6)));
+		printf(" Source: %s",ether_ntoa((struct ether_addr*)(packet+6)));
 	else {
 		printf("[|Truncated]\n");
 		return;
 	}
 	if (len>=14) {
 		uint16_t type = htons(*(uint16_t*)(packet+12));
-		printf(" %04x\n",type);
+		printf(" Ethertype: %04x\n",type);
 		decode_next(packet+14,len-14,"eth",type);
 	}
 	else {
