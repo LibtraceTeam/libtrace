96ddd3c3582a4aa573264f53efc15c2b9709f738
Perry Lorier <perry@waikato.ac.nz>
2005-10-31 03:27:54 +0000

    Remove the old versions of tracedump (now in libpacketdump)

--- examples/tracedump/tracedump.cc
+++
@@ -1,6 +0,0 @@
-#include "tracedump.h"
-
-void per_packet(int link_type,char *data,int size)
-{
-	decode_next(data,size,"link",link_type);
-}
--- examples/tracedump/tracedump.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TRACEDUMP_H__
-#define __TRACEDUMP_H__
-
-void per_packet(int link_type,char *buffer, int size);
-
-void decode_next(char *packet,int len,char *proto_name,int type);
-
-#endif
