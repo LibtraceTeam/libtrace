5b669219e34de59f68938a46ca0008f9875e29ce
Perry Lorier <perry@waikato.ac.nz>
2006-02-28 00:44:47 +0000

    Test seek support

---
+++ test/test-seek.c
@@ -0,0 +1,94 @@
+/*
+ * This file is part of libtrace
+ *
+ * Copyright (c) 2004 The University of Waikato, Hamilton, New Zealand.
+ * Authors: Daniel Lawson 
+ *          Perry Lorier 
+ *          
+ * All rights reserved.
+ *
+ * This code has been developed by the University of Waikato WAND 
+ * research group. For further information please see http://www.wand.net.nz/
+ *
+ * libtrace is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libtrace is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libtrace; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+#include "dagformat.h"
+#include "libtrace.h"
+
+struct libtrace_t *trace;
+
+int main(int argc, char *argv[]) {
+        char *uri = "erf:traces/100_packets.erf";
+        int psize = 0;
+	int error = 0;
+	int count = 0;
+	libtrace_packet_t *packet;
+
+	trace = trace_create(uri);
+
+	trace_start(trace);
+
+	if (trace_seek_erf_timestamp(trace,4704246759960519168ULL)==-1) {
+		libtrace_err_t err=trace_get_err(trace);
+		printf("%s\n",err.problem);
+	}
+	
+	packet=trace_create_packet();
+        for (;;) {
+		if ((psize = trace_read_packet(trace, packet)) <0) {
+			error = 1;
+			break;
+		}
+		if (psize == 0) {
+			error = 0;
+			break;
+		}
+		count ++;
+        }
+	trace_destroy_packet(&packet);
+	if (error == 0) {
+		if (count == 4) {
+			printf("success: 4 packets read\n");
+		} else {
+			printf("failure: 4 packets expected, %d seen\n",count);
+			error = 1;
+		}
+	} else {
+		printf("failure: %s\n",trace_get_err(trace).problem);
+	}
+        trace_destroy(trace);
+        return error;
+}
