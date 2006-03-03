7dda48adfcf1b1bf3f6a9b6fa053a82011834b36
Perry Lorier <perry@waikato.ac.nz>
2006-03-03 04:26:47 +0000

    Make the testcase clean up after itself

--- tools/tracemerge/test-tracemerge.sh
+++ tools/tracemerge/test-tracemerge.sh
@@ -2,4 +2,6 @@
 
 ./tracemerge erf:trace1.erf \
 	erf:../../test/traces/100_packets.erf \
-	pcap:../../test/traces/100_packets.pcap
+	pcap:../../test/traces/100_packets.pcap || exit 1
+
+rm -f trace1.erf
