1f21c79758defbd903048e4e579e9f89a7147ae3
Matt Brown <mattb@wand.net.nz>
2006-04-27 10:12:53 +0000

    Added winpcap support to the Visual Studio infrastructure. Until we get some sort of autoconf replacement winpcap is a hard requirement unless you manually edit config-win.h to remove the HAVE_PCAP, HAVE_BPF, etc directives.

--- config-win.h
+++ config-win.h
@@ -1,13 +1,11 @@
-#
-# Highly hacked config.h template for WIN32 builds.
-#
-# This really needs to be fixed better sometime...
-#
-# Matt Brown <matt@mattb.net.nz>
-#
-
 #define PACKAGE_VERSION "3.0.0"
 
 #define HAVE__STRNICMP 1
 
 #define HAVE_SPRINTF_S 1
+
+#define HAVE_PCAP 1
+#define HAVE_BPF 1
+#define HAVE_PCAP_BPF 1
+#define HAVE_PCAP_H 1
+#define HAVE_PCAP_SENDPACKET 1
