73ef8ce420435553bb1e8afa2bbedfa276eacce7
Perry Lorier <perry@waikato.ac.nz>
2006-05-23 01:20:22 +0000

    Make sure all things are declared Move the declarations to alphabetical order.

--- tools/tracereport/report.h
+++ tools/tracereport/report.h
@@ -1,17 +1,20 @@
 #ifndef REPORT_H
 #define REPORT_H
 
-void tos_per_packet(struct libtrace_packet_t *packet);
-void port_per_packet(struct libtrace_packet_t *packet);
-void error_per_packet(struct libtrace_packet_t *packet);
-void protocol_per_packet(struct libtrace_packet_t *packet);
-void ttl_per_packet(struct libtrace_packet_t *packet);
 void dir_per_packet(struct libtrace_packet_t *packet);
+void error_per_packet(struct libtrace_packet_t *packet);
+void flow_per_packet(struct libtrace_packet_t *packet);
+void port_per_packet(struct libtrace_packet_t *packet);
+void protocol_per_packet(struct libtrace_packet_t *packet);
+void tos_per_packet(struct libtrace_packet_t *packet);
+void ttl_per_packet(struct libtrace_packet_t *packet);
 
+void dir_report(void);
 void error_report(void);
+void flow_report(void);
+void port_report(void);
 void protocol_report(void);
 void tos_report(void);
-void port_report(void);
-void dir_report(void);
+void ttl_report(void);
 
 #endif
