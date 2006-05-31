bd6869b04bbe1488ca91a7e5d34177b123d4a2a8
Perry Lorier <perry@waikato.ac.nz>
2006-05-31 11:43:18 +0000

    Fix 64bit issues

--- libpacketdump/link_4.cc
+++ libpacketdump/link_4.cc
@@ -42,7 +42,7 @@ void decode(int link_type,char *packet,int len)
 	printf(" %s",trace_ether_ntoa((uint8_t*)(hdr->mac2), ether_buf));
 	printf(" %s",trace_ether_ntoa((uint8_t*)(hdr->mac3), ether_buf));
 
-	struct ieee_802_11_payload *pld = (struct ieee_802_11_payload *) ((int)packet + sizeof(struct ieee_802_11_header) - 2);
+	struct ieee_802_11_payload *pld = (struct ieee_802_11_payload *) ((char*)packet + sizeof(struct ieee_802_11_header) - 2);
 	uint16_t type = htons(pld->type);
 	printf(" %04x\n",type);
 	decode_next((char *)pld->data,len-(sizeof(struct ieee_802_11_header)),"eth",type);
