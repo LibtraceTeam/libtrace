d3fd4d3b16ea1244a5e88c33caff5888da312845
Perry Lorier <perry@waikato.ac.nz>
2006-06-23 00:01:36 +0000

    Move pcapfile_pkt_hdr_t to be a global libtrace internal structure Change the way detecting trace types works

--- lib/format_pcapfile.c
+++ lib/format_pcapfile.c
@@ -43,9 +43,7 @@
 
 #define DATA(x) ((struct pcapfile_format_data_t*)((x)->format_data))
 
-struct pcapfile_format_data_t {
-	libtrace_io_t *file;
-	struct pcapfile_header_t {
+typedef struct pcapfile_header_t {
 		uint32_t magic_number;   /* magic number */
 		uint16_t version_major;  /* major version number */
 		uint16_t version_minor;  /* minor version number */
@@ -53,7 +51,16 @@ struct pcapfile_format_data_t {
 		uint32_t sigfigs;        /* timestamp accuracy */
 		uint32_t snaplen;        /* aka "wirelen" */
 		uint32_t network;        /* data link type */
-	} header;
+} pcapfile_header_t; 
+
+struct pcapfile_format_data_t {
+	libtrace_io_t *file;
+	pcapfile_header_t header;
+};
+
+struct pcapfile_format_data_out_t {
+	libtrace_io_t *file;
+
 };
 
 static int pcapfile_init_input(libtrace_t *libtrace) {
@@ -146,13 +153,6 @@ static int pcapfile_fin_input(libtrace_t *libtrace)
 	return 0; /* success */
 }
 
-struct pcapfile_pkt_hdr_t {
-	uint32_t ts_sec;
-	uint32_t ts_usec;
-	uint32_t caplen;
-	uint32_t wirelen;
-};
-
 static int pcapfile_read_packet(libtrace_t *libtrace, libtrace_packet_t *packet) {
 	int err;
 
@@ -168,7 +168,7 @@ static int pcapfile_read_packet(libtrace_t *libtrace, libtrace_packet_t *packet)
 
 	err=libtrace_io_read(DATA(libtrace)->file,
 			packet->buffer,
-			sizeof(struct pcapfile_pkt_hdr_t));
+			sizeof(libtrace_pcapfile_pkt_hdr_t));
 
 	if (err<0) {
 		trace_set_err(libtrace,errno,"reading packet");
@@ -183,8 +183,8 @@ static int pcapfile_read_packet(libtrace_t *libtrace, libtrace_packet_t *packet)
 
 
 	err=libtrace_io_read(DATA(libtrace)->file,
-			(char*)packet->buffer+sizeof(struct pcapfile_pkt_hdr_t),
-			swapl(libtrace,((struct pcapfile_pkt_hdr_t*)packet->buffer)->caplen)
+			(char*)packet->buffer+sizeof(libtrace_pcapfile_pkt_hdr_t),
+			swapl(libtrace,((libtrace_pcapfile_pkt_hdr_t*)packet->buffer)->caplen)
 			);
 
 	
@@ -197,22 +197,42 @@ static int pcapfile_read_packet(libtrace_t *libtrace, libtrace_packet_t *packet)
 	}
 
 	packet->payload = (char*)packet->buffer 
-		+ sizeof(struct pcapfile_pkt_hdr_t);
+		+ sizeof(libtrace_pcapfile_pkt_hdr_t);
 	
-	return sizeof(struct pcapfile_pkt_hdr_t)
-		+swapl(libtrace,((struct pcapfile_pkt_hdr_t*)packet->buffer)->caplen);
+	return sizeof(libtrace_pcapfile_pkt_hdr_t)
+		+swapl(libtrace,((libtrace_pcapfile_pkt_hdr_t*)packet->buffer)->caplen);
 }
+
+#if 0
+static void pcapfile_write_packet(libtrace_out_t *out,
+		const libtrace_packet_t *packet)
+{
+	struct pcapfile_pkt_hdr_t hdr;
+
+	tv = trace_get_timeval(packet);
+	hdr.ts_sec = tv.tv_sec;
+	hdr.ts_usec = tv.tv_usec;
+	hdr.caplen = trace_get_capture_length(packet);
+	hdr.wirelen = trace_get_wire_length(packet);
+
+	write(fd,&hdr,sizeof(hdr));
+	write(fd,packet->payload,hdr.caplen);
+	
+}
+#endif
 
 
 static libtrace_linktype_t pcapfile_get_link_type(
 		const libtrace_packet_t *packet) 
 {
-
+#if 0
 	return pcap_dlt_to_libtrace(
 			swapl(packet->trace,
 				DATA(packet->trace)->header.network
 			     )
 			);
+#endif
+	return pcap_dlt_to_libtrace(rt_to_pcap_dlt(packet->type));
 }
 
 static libtrace_direction_t pcapfile_get_direction(const libtrace_packet_t *packet) 
@@ -278,8 +298,8 @@ static libtrace_direction_t pcapfile_get_direction(const libtrace_packet_t *pack
 static struct timeval pcapfile_get_timeval(
 		const libtrace_packet_t *packet) 
 {
-	struct pcapfile_pkt_hdr_t *hdr =
-		(struct pcapfile_pkt_hdr_t*)packet->header;
+	libtrace_pcapfile_pkt_hdr_t *hdr =
+		(libtrace_pcapfile_pkt_hdr_t*)packet->header;
 	struct timeval ts;
 	ts.tv_sec = swapl(packet->trace,hdr->ts_sec);
 	ts.tv_usec = swapl(packet->trace,hdr->ts_usec);
@@ -288,15 +308,15 @@ static struct timeval pcapfile_get_timeval(
 
 
 static int pcapfile_get_capture_length(const libtrace_packet_t *packet) {
-	struct pcapfile_pkt_hdr_t *pcapptr 
-		= (struct pcapfile_pkt_hdr_t *)packet->header;
+	libtrace_pcapfile_pkt_hdr_t *pcapptr 
+		= (libtrace_pcapfile_pkt_hdr_t *)packet->header;
 
 	return swapl(packet->trace,pcapptr->caplen);
 }
 
 static int pcapfile_get_wire_length(const libtrace_packet_t *packet) {
-	struct pcapfile_pkt_hdr_t *pcapptr 
-		= (struct pcapfile_pkt_hdr_t *)packet->header;
+	libtrace_pcapfile_pkt_hdr_t *pcapptr 
+		= (libtrace_pcapfile_pkt_hdr_t *)packet->header;
 	if (packet->type==pcap_dlt_to_rt(TRACE_DLT_EN10MB))
 		/* Include the missing FCS */
 		return swapl(packet->trace,pcapptr->wirelen)+4; 
@@ -305,17 +325,17 @@ static int pcapfile_get_wire_length(const libtrace_packet_t *packet) {
 }
 
 static int pcapfile_get_framing_length(const libtrace_packet_t *packet UNUSED) {
-	return sizeof(struct pcapfile_pkt_hdr_t);
+	return sizeof(libtrace_pcapfile_pkt_hdr_t);
 }
 
 static size_t pcapfile_set_capture_length(libtrace_packet_t *packet,size_t size) {
-	struct pcapfile_pkt_hdr_t *pcapptr = 0;
+	libtrace_pcapfile_pkt_hdr_t *pcapptr = 0;
 	assert(packet);
 	if (size > trace_get_capture_length(packet)) {
 		/* can't make a packet larger */
 		return trace_get_capture_length(packet);
 	}
-	pcapptr = (struct pcapfile_pkt_hdr_t *)packet->header;
+	pcapptr = (libtrace_pcapfile_pkt_hdr_t *)packet->header;
 	pcapptr->caplen = swapl(packet->trace,size);
 	return trace_get_capture_length(packet);
 }
