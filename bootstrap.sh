32e3f075d2011fb63eda055ce899cd00fb2c7fcd
Perry Lorier <perry@waikato.ac.nz>
2006-08-21 12:26:21 +0000

    Deal with darwins renaming of libtoolize -> glibtoolize

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,18 @@
 #! /bin/sh
 
 set -x
+# Prefer aclocal 1.9 if we can find it
 aclocal-1.9 -I m4 || 
 	aclocal  -I m4
-libtoolize --force --copy
+
+# Darwin bizarrely uses glibtoolize
+libtoolize --force --copy ||
+	glibtoolize --force --copy
+
 autoheader
+
+# Prefer automake-1.9 if we can find it
 automake-1.9 --add-missing --copy --foreign || 
 	automake --add-missing --copy --foreign
+
 autoconf
