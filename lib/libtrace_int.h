94af8859b775e6714a97cc2522349ca1f9c1e2c1
Perry Lorier <perry@waikato.ac.nz>
2006-08-21 09:04:59 +0000

    Add new API for dealing with Big endian or Little Endian numbers on the wrong endian machine.

--- lib/libtrace_int.h
+++ lib/libtrace_int.h
@@ -360,6 +360,52 @@ char libtrace_to_erf_type(libtrace_linktype_t linktype);
 void promote_packet(libtrace_packet_t *packet);
 bool demote_packet(libtrace_packet_t *packet);
 
+
+uint64_t byteswap64(uint64_t num);
+uint32_t byteswap32(uint32_t num);
+uint16_t byteswap16(uint16_t num);
+
+/* Because some traces/protocols are defined as
+ * being "big endian" or "little endian" we have
+ * this series of macros.
+ */
+#if BYTE_ORDER == BIG_ENDIAN
+#define bswap_host_to_be64(num) ((uint64_t)(num))
+#define bswap_host_to_le64(num) byteswap64(num)
+#define bswap_host_to_be32(num) ((uint32_t)(num))
+#define bswap_host_to_le32(num) byteswap32(num)
+#define bswap_host_to_be16(num) ((uint16_t)(num))
+#define bswap_host_to_le16(num) byteswap16(num)
+
+#define bswap_be_to_host64(num) ((uint64_t)(num))
+#define bswap_le_to_host64(num) byteswap64(num)
+#define bswap_be_to_host32(num) ((uint32_t)(num))
+#define bswap_le_to_host32(num) byteswap32(num)
+#define bswap_be_to_host16(num) ((uint16_t)(num))
+#define bswap_le_to_host16(num) byteswap16(num)
+
+/* We use ntoh*() here, because the compiler may
+ * attempt to optimise it
+ */
+#elif BYTE_ORDER == LITTLE_ENDIAN
+#define bswap_host_to_be64(num) (byteswap64(num))
+#define bswap_host_to_le64(num) ((uint64_t)(num))
+#define bswap_host_to_be32(num) (htonl(num))
+#define bswap_host_to_le32(num) ((uint32_t)(num))
+#define bswap_host_to_be16(num) (htons(num))
+#define bswap_host_to_le16(num) ((uint16_t)(num))
+
+#define bswap_be_to_host64(num) (byteswap64(num))
+#define bswap_le_to_host64(num) ((uint64_t)(num))
+#define bswap_be_to_host32(num) (ntohl(num))
+#define bswap_le_to_host32(num) ((uint32_t)(num))
+#define bswap_be_to_host16(num) (ntohs(num))
+#define bswap_le_to_host16(num) ((uint16_t)(num))
+
+#else
+#error "Unknown byte order"
+#endif
+
 #ifdef HAVE_BPF
 /* A type encapsulating a bpf filter
  * This type covers the compiled bpf filter, as well as the original filter
