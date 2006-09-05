939bea76f588cbee3f2b3f805c17bc3d97c1f45b
Perry Lorier <perry@waikato.ac.nz>
2006-09-05 04:50:10 +0000

    Be more verbose about warnings (so we can pinpoint what's going on faster)

--- lib/format_linux.c
+++ lib/format_linux.c
@@ -309,7 +309,7 @@ static libtrace_linktype_t linuxnative_get_link_type(const struct libtrace_packe
 		case ARPHRD_PPP:
 			return TRACE_TYPE_NONE;
 		default: /* shrug, beyond me! */
-			printf("unknown type %x\n",linktype);
+			printf("unknown Linux ARPHRD type %x\n",linktype);
 			return (libtrace_linktype_t)~0U;
 	}
 }
