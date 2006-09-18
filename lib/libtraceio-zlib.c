eecda9f20aedc996152d3bef0ca645b817efe136
Perry Lorier <perry@waikato.ac.nz>
2006-09-18 11:10:56 +0000

    Make splint happier

--- lib/libtraceio-zlib.c
+++ lib/libtraceio-zlib.c
@@ -13,7 +13,7 @@ ssize_t libtrace_io_read(libtrace_io_t *io, void *buf, size_t len)
 	int err2=errno;
 	if (err>=0) {
 		/* successfully read <x> bytes */
-		return err;
+		return (ssize_t)err;
 	}
 	switch(err) {
 		case Z_STREAM_END:
@@ -35,6 +35,8 @@ ssize_t libtrace_io_read(libtrace_io_t *io, void *buf, size_t len)
 libtrace_io_t *libtrace_io_fdopen(int fd, const char *mode)
 {
 	libtrace_io_t *io = (libtrace_io_t*)malloc(sizeof(libtrace_io_t));
+	if (io == NULL)
+		return NULL;
 	io->file = gzdopen(fd,mode);
 	return io;
 }
@@ -42,6 +44,8 @@ libtrace_io_t *libtrace_io_fdopen(int fd, const char *mode)
 libtrace_io_t *libtrace_io_open(const char *path, const char *mode)
 {
 	libtrace_io_t *io = (libtrace_io_t*)malloc(sizeof(libtrace_io_t));
+	if (io == NULL)
+		return NULL;
 	io->file = gzopen(path,mode);
 	return io;
 }
@@ -51,14 +55,14 @@ libtrace_io_t *libtrace_io_open(const char *path, const char *mode)
  */
 void libtrace_io_close(libtrace_io_t *io)
 {
-	gzclose(io->file);
+	(void)gzclose(io->file);
 	io->file=NULL;
 	free(io);
 }
 
 ssize_t libtrace_io_write(libtrace_io_t *io, const void *buf, size_t len)
 {
-	return gzwrite(io->file,buf,len);
+	return (ssize_t)gzwrite(io->file,buf,len);
 }
 
 off_t libtrace_io_seek(libtrace_io_t *io, off_t offset, int whence)
@@ -68,5 +72,5 @@ off_t libtrace_io_seek(libtrace_io_t *io, off_t offset, int whence)
 
 ssize_t libtrace_io_tell(libtrace_io_t *io)
 {
-	return gztell(io->file);
+	return (ssize_t)gztell(io->file);
 }
