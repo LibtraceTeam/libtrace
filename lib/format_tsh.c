6bea4359c0030941dc379ce8b87000fa0aa39139
Perry Lorier <perry@waikato.ac.nz>
2007-04-21 03:58:06 +0000

    Add support for the fr+ trace format

--- lib/format_tsh.c
+++ lib/format_tsh.c
@@ -23,7 +23,7 @@
  * along with libtrace; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: format_erf.c 1163 2007-03-23 04:41:03Z perry $
+ * $Id$
  *
  */
 #include "config.h"
@@ -179,7 +179,7 @@ static int tsh_get_wire_length(const libtrace_packet_t *packet) {
 }
 
 static void tsh_help(void) {
-	printf("tsh format module: $Revision: 1163 $\n");
+	printf("tsh format module: $Revision$\n");
 	printf("Supported input URIs:\n");
 	printf("\ttsh:/path/to/file\t(uncompressed)\n");
 	printf("\ttsh:/path/to/file.gz\t(gzip-compressed)\n");
@@ -192,7 +192,7 @@ static void tsh_help(void) {
 
 static struct libtrace_format_t tshformat = {
 	"tsh",
-	"$Id: format_erf.c 1163 2007-03-23 04:41:03Z perry $",
+	"$Id$",
 	TRACE_FORMAT_TSH,
 	tsh_init_input,			/* init_input */	
 	NULL,				/* config_input */
@@ -225,7 +225,47 @@ static struct libtrace_format_t tshformat = {
 	NULL				/* next pointer */
 };
 
+/* the tsh header format is the same as tsh, except that the bits that will
+ * always be "0" in the fr+ format are used for an "interface" identifier,
+ * thus on tr+ traces, this will always be 0.  So, we use the exact same
+ * decoder for both traces.
+ */
+static struct libtrace_format_t frplusformat = {
+	"fr+",
+	"$Id$",
+	TRACE_FORMAT_TSH,
+	tsh_init_input,			/* init_input */	
+	NULL,				/* config_input */
+	tsh_start_input,		/* start_input */
+	NULL,				/* pause_input */
+	NULL,				/* init_output */
+	NULL,				/* config_output */
+	NULL,				/* start_output */
+	tsh_fin_input,			/* fin_input */
+	NULL,				/* fin_output */
+	tsh_read_packet,		/* read_packet */
+	NULL,				/* fin_packet */
+	NULL,				/* write_packet */
+	tsh_get_link_type,		/* get_link_type */
+	tsh_get_direction,		/* get_direction */
+	NULL,				/* set_direction */
+	NULL,				/* get_erf_timestamp */
+	tsh_get_timeval,		/* get_timeval */
+	NULL,				/* get_seconds */
+	NULL,				/* seek_erf */
+	NULL,				/* seek_timeval */
+	NULL,				/* seek_seconds */
+	tsh_get_capture_length,		/* get_capture_length */
+	tsh_get_wire_length,		/* get_wire_length */
+	tsh_get_framing_length,		/* get_framing_length */
+	NULL,				/* set_capture_length */
+	NULL,				/* get_fd */
+	trace_event_trace,		/* trace_event */
+	tsh_help,			/* help */
+	NULL				/* next pointer */
+};
 
 void tsh_constructor(void) {
 	register_format(&tshformat);
+	register_format(&frplusformat);
 }
