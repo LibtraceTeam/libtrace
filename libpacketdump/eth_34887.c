c5f3398f685c6f80a94df0fd5efae7f106ce1952
Shane Alcock <salcock@waikato.ac.nz>
2008-06-16 22:49:48 +0000

    * Fixed numerous bugs in the MPLS libpacketdump decoder

--- libpacketdump/eth_34887.c
+++ libpacketdump/eth_34887.c
@@ -4,32 +4,42 @@
 #define LE(lhs,n) 						\
 	do {							\
 		uint64_t num=0;					\
+		int size=0;					\
 		if ((offset+n)>len*8) return;			\
-		if (n>16)					\
+		if (n>16) {					\
 			num=htonl(*(uint32_t*)(packet+offset/8));\
-		else if (n>8)					\
+			size = 32;\
+		} else if (n>8) {				\
 			num=htons(*(uint16_t*)(packet+offset/8));\
-		else 						\
+			size = 16;				\
+		} else { 					\
 			num=*(uint8_t*)(packet+offset/8);	\
-		num=num>>(32-n);				\
+			size = 8;				\
+		}						\
+		num=num>>(size - (n + (offset % 8)));		\
 		offset+=n;					\
-		lhs=num&((1<<(n+1))-1);				\
+		lhs=num&((1<<(n))-1);				\
 	} while(0)
 
 void decode(int link_type,const char *packet,unsigned len)
 {
 	int offset=0;
 	int value;
-	LE(value,20); 	printf(" MPLS: Label: %d\n",offset);
-	LE(value,3); 	printf(" MPLS: Class of service: %d\n",offset);
-	LE(value,1);	printf(" MPLS: Stack: %s\n",offset?"Last" :"More");
-	LE(value,8);	printf(" MPLS: TTL: %d\n",offset);
+	int more = 0;
+	LE(value,20); 	printf(" MPLS: Label: %d\n",value);
+	LE(value,3); 	printf(" MPLS: Class of service: %d\n",value);
+	LE(value,1);	printf(" MPLS: Stack: %s\n",value?"Last" :"More");
+	if (value == 0) more = 1;
+	LE(value,8);	printf(" MPLS: TTL: %d\n",value);
+	
 	/* MPLS doesn't say what it's encapsulating, so we make an educated
 	 * guess and pray.
 	 */
-	if ((*(packet+32)&0xF0) == 0x40)
+	if (more)
+		decode_next(packet+offset/8,len-4,"eth",0x8847);
+	else if ((*(packet+4)&0xF0) == 0x40)
 		decode_next(packet+offset/8,len-4,"eth",0x0800);
-	else if ((*(packet+32)&0xF0) == 0x60)
+	else if ((*(packet+4)&0xF0) == 0x60)
 		decode_next(packet+offset/8,len-4,"eth",0x86DD);
 	else
 		decode_next(packet+offset/8,len-4,"link",1);
