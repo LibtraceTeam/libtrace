3fc79487e2833295cba6f44737ffeea89b87aa07
Shane Alcock <salcock@waikato.ac.nz>
2009-06-10 04:21:33 +0000

    Major rewrite of trace_event_device  * Replaced FIONREAD ioctl with a select() to check whether data is available. The select() is probably slower but the ioctl was not working for newer versions of pcap under linux, due to the use of mmaped packet sockets which would not report the expected value for that ioctl.  * Added checks for the return value of trace_read_packet so that we can catch any error or EOF events that occur rather than always returning a PACKET event
    Thanks to Benjamin Black for bringing this to our attention.

--- lib/format_helper.c
+++ lib/format_helper.c
@@ -40,6 +40,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
 #include "format_helper.h"
 
 #include <assert.h>
@@ -65,24 +66,59 @@ struct libtrace_eventobj_t trace_event_device(struct libtrace_t *trace, struct l
 struct libtrace_eventobj_t trace_event_device(struct libtrace_t *trace, 
 					struct libtrace_packet_t *packet) {
 	struct libtrace_eventobj_t event = {0,0,0.0,0};
-	int data;
+
+	fd_set rfds, rfds_param;
+	int ret;
+	int max_fd;
+	struct timeval tv;
 
 	assert(trace != NULL);
 	assert(packet != NULL);
 	
+	FD_ZERO(&rfds);
+	FD_ZERO(&rfds_param);
+
 	if (trace->format->get_fd) {
 		event.fd = trace->format->get_fd(trace);
+		FD_SET(event.fd, &rfds);
+		max_fd = event.fd;
 	} else {
 		event.fd = 0;
+		max_fd = -1;
 	}
-	if (ioctl(event.fd,FIONREAD,&data)==-1) {
-		event.type = TRACE_EVENT_TERMINATE;
-		return event;
-	} 
-		
-	if (data>0) {
+
+	/* Use select() to perform a quick poll to check that there is data
+	 * available - we used to use FIONREAD here but that does not work
+	 * for mmapped pcap sockets. As recent pcap on linux (e.g. Ubuntu 9.04)
+	 * uses mmapped sockets by default, I've switched over to this 
+	 * solution. */
+
+	do {
+		tv.tv_sec = 0;
+		tv.tv_usec = 0;
+		rfds_param = rfds;
+
+		ret = select(max_fd + 1, &rfds_param, NULL, NULL, &tv);
+		if (ret == -1 && errno != EINTR) {
+			event.type = TRACE_EVENT_TERMINATE;
+			return event;
+		}
+	} while (ret == -1);
+
+	if (FD_ISSET(event.fd, &rfds_param)) {
 		event.size = trace_read_packet(trace,packet);
-		event.type = TRACE_EVENT_PACKET;
+		
+		if (event.size < 1) {
+			/* Covers error and EOF events - terminate rather 
+			 * than report a packet as available */
+			if (trace_is_err(trace)) {
+				trace_perror(trace, "read packet");
+			}
+			event.type = TRACE_EVENT_TERMINATE;
+		} else {
+
+			event.type = TRACE_EVENT_PACKET;
+		}
 		return event;
 	}
 	event.type= TRACE_EVENT_IOWAIT;
