2b4316b312dd1965ec7fad3621a7915a69e2c202
Perry Lorier <perry@waikato.ac.nz>
2010-03-21 21:23:50 +0000

    Compile if O_DIRECT isn't available (MacOS sigh)

--- lib/ior-stdio.c
+++ lib/ior-stdio.c
@@ -60,7 +60,12 @@ io_t *stdio_open(const char *filename)
 	if (strcmp(filename,"-") == 0)
 		DATA(io)->fd = 0; /* STDIN */
 	else
-		DATA(io)->fd = open(filename,O_RDONLY|(force_directio_read?O_DIRECT:0));
+		DATA(io)->fd = open(filename,
+			O_RDONLY
+#ifdef O_DIRECT
+			|(force_directio_read?O_DIRECT:0
+#endif
+			));
 	io->source = &stdio_source;
 
 	if (DATA(io)->fd == -1) {
