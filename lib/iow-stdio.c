22cdd399944f847ba5f7141f8fc52acfc8e57d25
Perry Lorier <perry@waikato.ac.nz>
2010-03-30 01:30:48 +0000

    Correctly accumulate data into large buffers for write

--- lib/iow-stdio.c
+++ lib/iow-stdio.c
@@ -117,38 +117,66 @@ iow_t *stdio_wopen(const char *filename)
  */
 static off_t stdio_wwrite(iow_t *iow, const char *buffer, off_t len)
 {
-	struct iovec iov[2];
-	int err;
+	int towrite = len;
 	/* Round down size to the nearest multiple of MIN_WRITE_SIZE */
 
-	assert(len >= 0);
-	
-	if (DATA(iow)->offset + len >= MIN_WRITE_SIZE) {
-		int amount;
-		iov[0].iov_base = DATA(iow)->buffer;
-		iov[0].iov_len = DATA(iow)->offset;
-		iov[1].iov_base = (void*)buffer; /* cast away constness, which is safe here */
-		iov[1].iov_len = len - (DATA(iow)->offset+len) % MIN_WRITE_SIZE;
-		err=writev(DATA(iow)->fd, iov, 2);
+	assert(towrite >= 0);
+
+	while (DATA(iow)->offset + towrite >= MIN_WRITE_SIZE) {
+		int err;
+		struct iovec iov[2];
+		int total = towrite - (DATA(iow)->offset+towrite) % MIN_WRITE_SIZE;
+		int amount = total;
+		int count=0;
+		if (DATA(iow)->offset) {
+			iov[count].iov_base = DATA(iow)->buffer;
+			iov[count].iov_len = min(DATA(iow)->offset,amount);
+			amount -= iov[count].iov_len;
+			++count;
+		}
+		if (towrite) {
+			iov[count].iov_base = (void*)buffer; 	/* cast away constness, which is safe 
+								 * here 
+								 */
+			iov[count].iov_len = amount;
+			amount -= iov[count].iov_len;
+			++count;
+		}
+		assert(amount == 0);
+		err=writev(DATA(iow)->fd, iov, count);
 		if (err==-1)
 			return -1;
+		if (err != total)
+			fprintf(stderr,"Wrote %d/%d\n",err,total);
+
 		/* Drop off "err" bytes from the beginning of the buffers */
-		amount = min(DATA(iow)->offset, err);
+		amount = min(DATA(iow)->offset, err); /* How much we took out of the buffer */
+		memmove(DATA(iow)->buffer, 
+			DATA(iow)->buffer+amount,
+			DATA(iow)->offset-amount);
 		DATA(iow)->offset -= amount;
-		err -= amount;
+
+		err -= amount; /* How much was written */
+
+		assert(err <= towrite);
+
 		buffer += err;
-		len -= err;
+		towrite -= err;
+
+		assert(DATA(iow)->offset == 0);
 	}
 
 	/* Make sure we're not going to overflow the buffer.  The above writev should assure
  	 * that this is true
  	 */
-	assert(DATA(iow)->offset + len <= MIN_WRITE_SIZE);
-	assert(len >= 0);
+	assert(DATA(iow)->offset + towrite <= MIN_WRITE_SIZE);
+	assert(towrite >= 0);
 
-	/* Copy the remainder into the buffer to write next time. */
-	memcpy(DATA(iow)->buffer + DATA(iow)->offset, buffer, len);
-	DATA(iow)->offset += len;
+	if (towrite > 0) {
+		/* Copy the remainder into the buffer to write next time. */
+		memcpy(DATA(iow)->buffer + DATA(iow)->offset, buffer, towrite);
+		DATA(iow)->offset += towrite;
+	}
 
 	return len;
 }
@@ -164,9 +192,9 @@ static void stdio_wclose(iow_t *iow)
 	if (err != -1 && (err & O_DIRECT) != 0) {
 		fcntl(DATA(iow)->fd,F_SETFL, err & ~O_DIRECT);
 	}
-	write(DATA(iow)->fd, DATA(iow)->buffer, DATA(iow)->offset);
-	DATA(iow)->offset = 0;
 #endif
+	err=write(DATA(iow)->fd, DATA(iow)->buffer, DATA(iow)->offset);
+	DATA(iow)->offset = 0;
 	close(DATA(iow)->fd);
 	free(iow->data);
 	free(iow);
