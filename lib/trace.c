b185327baf45b87a817b266965b92428aa275206
Shane Alcock <salcock@waikato.ac.nz>
2010-07-15 04:53:37 +0000

    * Set the value of remaining to the minimum of the wire and capture lengths when calling trace_get_packet_buffer()

--- lib/trace.c
+++ lib/trace.c
@@ -816,9 +816,36 @@ DLLEXPORT int trace_write_packet(libtrace_out_t *libtrace, libtrace_packet_t *pa
 /* Get a pointer to the first byte of the packet payload */
 DLLEXPORT void *trace_get_packet_buffer(const libtrace_packet_t *packet,
 		libtrace_linktype_t *linktype, uint32_t *remaining) {
+	int cap_len;
+	int wire_len;
+
 	assert(packet != NULL);
 	if (linktype) *linktype = trace_get_link_type(packet);
-	if (remaining) *remaining = trace_get_capture_length(packet);
+	if (remaining) {
+		/* I think we should choose the minimum of the capture and
+		 * wire lengths to be the "remaining" value. If the packet has
+		 * been padded to increase the capture length, we don't want
+		 * to allow subsequent protocol decoders to consider the 
+		 * padding as part of the packet.
+		 *
+		 * For example, in Auck 4 there is a trace where the IP header
+		 * length is incorrect (24 bytes) followed by a 20 byte TCP
+		 * header. Total IP length is 40 bytes. As a result, the
+		 * legacyatm padding gets treated as the "missing" bytes of
+		 * the TCP header, which isn't the greatest. We're probably
+		 * better off returning an incomplete TCP header in that case.
+		 */
+		
+		cap_len = trace_get_capture_length(packet);
+		wire_len = trace_get_wire_length(packet);
+
+		assert(cap_len >= 0 && wire_len >= 0);
+		if (wire_len < cap_len)
+			*remaining = wire_len;
+		else
+			*remaining = cap_len;
+		/* *remaining = trace_get_capture_length(packet); */
+	}
 	return (void *) packet->payload;
 }
 
