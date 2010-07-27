fade02bcb02e2671ef5ad0dcdf2b1dbb3583bdfd
Shane Alcock <salcock@waikato.ac.nz>
2010-07-27 05:01:46 +0000

    * Fix strict-aliasing warning in bitbuffer.c  * Fixed warnings about unused functions in the lexer code

--- libpacketdump/bitbuffer.c
+++ libpacketdump/bitbuffer.c
@@ -16,6 +16,8 @@ static bitbuffer_t getbit(void **packet, int *packlen, uint64_t numbits)
 {
     bitbuffer_t ret;
     bitbuffer_t mask;
+    
+    char *pktptr = NULL;
 
     /* While the buffer is not filled up and there is still
      * data in the packet to read, read a byte...
@@ -29,7 +31,11 @@ static bitbuffer_t getbit(void **packet, int *packlen, uint64_t numbits)
 	byte=(*((bitbuffer_t*)*packet))&0xff;
 	buffer |= (bitbuffer_t)byte << (sizeof(bitbuffer_t)*8-(bits+sizeof(byte)*8));
 	/* update the position within the packet */
-	*packet = ((char*)*packet) + 1;
+	pktptr = (char *)*packet;
+	pktptr += 1;
+	*packet = pktptr;
+
+	//*packet = ((char*)*packet) + 1;
 
 	bits += sizeof(byte)*8;
 	*packlen -= 1;
