44028d4384ea1249d683a592bae927b7a75ac83c
Shane Alcock <salcock@waikato.ac.nz>
2011-07-21 01:02:35 +0000

    * Fixed problem that Nevil was having with building on Fedora Core 15

--- lib/format_dag25.c
+++ lib/format_dag25.c
@@ -46,6 +46,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #include <sys/mman.h>
 /* XXX: Windows doesn't have pthreads, but this code doesn't compile under
