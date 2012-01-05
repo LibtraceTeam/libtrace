cc6fcee3c01a9eea3ea570db0fde9c5a7b64eb93
Shane Alcock <salcock@waikato.ac.nz>
2012-01-05 22:25:39 +0000

    * Added a function for parsing OSPF packets that contain LSA headers only, e.g. LS Acks, as we can't rely on the LSA length field to tell us how long the LSA in these packets is :(

--- lib/protocols_transport.c
+++ lib/protocols_transport.c
@@ -352,6 +352,57 @@ DLLEXPORT int trace_get_next_ospf_link_v2(unsigned char **current,
 
 }
 
+DLLEXPORT int trace_get_next_ospf_lsa_header_v2(unsigned char **current,
+		libtrace_ospf_lsa_v2_t **lsa_hdr,
+                uint32_t *remaining,
+                uint8_t *lsa_type,
+                uint16_t *lsa_length) {
+	
+	int valid_lsa = 0;
+
+	if (*current == NULL || *remaining < sizeof(libtrace_ospf_lsa_v2_t)) {
+		*lsa_hdr = NULL;
+		*remaining = 0;
+		return 0;
+
+	}
+
+	*lsa_hdr = (libtrace_ospf_lsa_v2_t *)(*current);
+	
+	/* Check that the LSA type is valid */
+	switch ((*lsa_hdr)->lsa_type) {
+		case TRACE_OSPF_LS_ROUTER:
+		case TRACE_OSPF_LS_NETWORK:
+		case TRACE_OSPF_LS_SUMMARY:
+		case TRACE_OSPF_LS_ASBR_SUMMARY:
+		case TRACE_OSPF_LS_EXTERNAL:
+			valid_lsa = 1;
+			break;
+	}
+	
+	/* This function is for reading LSA headers only, e.g. those in DB desc
+	 * or LS Ack packets. As such, I'm going to set the type and length to
+	 * values that should prevent anyone from trying to treat subsequent
+	 * payload as an LSA body */
+	*lsa_type = 0;
+	*lsa_length = sizeof(libtrace_ospf_lsa_v2_t);
+
+	if (!valid_lsa) {
+		*remaining = 0;
+		return -1;
+	}
+	
+	*remaining -= *lsa_length;
+	*current += *lsa_length;
+
+	if (remaining == 0) {
+		/* No more LSAs */
+		return 0;
+	}
+
+	return 1;
+}
+
 DLLEXPORT int trace_get_next_ospf_lsa_v2(unsigned char **current,
 		libtrace_ospf_lsa_v2_t **lsa_hdr,
 		unsigned char **lsa_body,
@@ -391,6 +442,7 @@ DLLEXPORT int trace_get_next_ospf_lsa_v2(unsigned char **current,
 		 * If this occurs, you've probably managed to read something
 		 * that is NOT a legit LSA */
 		*remaining = 0;
+		*lsa_body = NULL;
 		return -1;
 	}
 	
