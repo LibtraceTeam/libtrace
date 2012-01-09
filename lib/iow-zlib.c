cc27c869f7188c2b75ef9ca963ce01ced5f9ed3c
Shane Alcock <salcock@waikato.ac.nz>
2012-01-09 01:20:33 +0000

    * Fixed bug where a zlib writer was not being flushed correctly during zlib_wclose(). The problem was that we were not handling the case where Z_BUF_ERROR is returned by the final deflate() call, instead jumping straight to closing as though we had gotten a Z_STREAM_END.
    This should fix that annoying problem where the occasionaly trace file written by libtrace reports "Truncated packet" when being read back in.

--- lib/iow-zlib.c
+++ lib/iow-zlib.c
@@ -39,6 +39,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 /* Libtrace IO module implementing a zlib writer */
 
@@ -141,14 +142,25 @@ static off_t zlib_wwrite(iow_t *iow, const char *buffer, off_t len)
 
 static void zlib_wclose(iow_t *iow)
 {
-	while (deflate(&DATA(iow)->strm, Z_FINISH) == Z_OK) {
-		/* Need to flush the output buffer */
+	int res;
+	
+	while (1) {
+		res = deflate(&DATA(iow)->strm, Z_FINISH);
+
+		if (res == Z_STREAM_END)
+			break;
+		if (res == Z_STREAM_ERROR) {
+			fprintf(stderr, "Z_STREAM_ERROR while closing output\n");
+			break;
+		}
+	
 		wandio_wwrite(DATA(iow)->child, 
 				(char*)DATA(iow)->outbuff,
 				sizeof(DATA(iow)->outbuff)-DATA(iow)->strm.avail_out);
 		DATA(iow)->strm.next_out = DATA(iow)->outbuff;
 		DATA(iow)->strm.avail_out = sizeof(DATA(iow)->outbuff);
 	}
+
 	deflateEnd(&DATA(iow)->strm);
 	wandio_wwrite(DATA(iow)->child, 
 			(char *)DATA(iow)->outbuff,
