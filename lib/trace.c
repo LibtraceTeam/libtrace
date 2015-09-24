6fac5db5cedcd79abaa39c2fdb39a4a901dece09
Shane Alcock <salcock@waikato.ac.nz>
2015-09-25 11:18:55 +1200

    Actually, don't free hasher_data
    We can't guarantee that the data was actually malloc'd, so we'll
    have to trust the user to free it themselves.

--- lib/trace.c
+++ lib/trace.c
@@ -732,9 +732,6 @@ DLLEXPORT void trace_destroy(libtrace_t *libtrace) {
         if (libtrace->reporter_cbs)
                 trace_destroy_callback_set(libtrace->reporter_cbs);
 
-        if (libtrace->hasher_data)
-                free(libtrace->hasher_data);
-	
 	if (libtrace->event.packet) {
 		/* Don't use trace_destroy_packet here - there is almost
 		 * certainly going to be another libtrace_packet_t that is
