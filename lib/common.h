8485d9989045c399838801968bcef1be7e0184ab
Shane Alcock <salcock@waikato.ac.nz>
2016-08-29 16:14:27 +1200

    Fix files I broke with my automatic relicensing...

--- lib/common.h
+++ lib/common.h
@@ -23,3 +23,19 @@
  *
  *
  */
+
+#ifndef COMMON_H
+#define COMMON_H 1
+
+#include "config.h"
+
+#ifdef __cplusplus
+#  define BEGIN_C_DECLS extern "C" {
+#  define END_C_DECLS   }
+#else /* !__cplusplus */
+#  define BEGIN_C_DECLS
+#  define END_C_DECLS
+#endif /* __cplusplus */
+
+
+#endif /* COMMON_H */
