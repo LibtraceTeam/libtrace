b94478f5382298319215cf39ca79b4d865d9e0bd
Shane Alcock <salcock@waikato.ac.nz>
2018-03-06 16:03:46 +1300

    Make SCB data structure part of the exported API

--- lib/data-struct/simple_circular_buffer.h
+++ lib/data-struct/simple_circular_buffer.h
@@ -1,6 +1,8 @@
 #ifndef LIBTRACE_SCB_H_
 #define LIBTRACE_SCB_H_
 
+#include "libtrace.h"
+
 typedef struct libtracescb {
         uint8_t *address;
         uint32_t count_bytes;
@@ -10,10 +12,13 @@ typedef struct libtracescb {
 } libtrace_scb_t;
 
 
-void libtrace_scb_init(libtrace_scb_t *buf, uint32_t size, uint16_t id);
-void libtrace_scb_destroy(libtrace_scb_t *buf);
-int libtrace_scb_recv_sock(libtrace_scb_t *buf, int sock, int recvflags);
-uint8_t *libtrace_scb_get_read(libtrace_scb_t *buf, uint32_t *available);
-void libtrace_scb_advance_read(libtrace_scb_t *buf, uint32_t forward);
+DLLEXPORT void libtrace_scb_init(libtrace_scb_t *buf, uint32_t size,
+                uint16_t id);
+DLLEXPORT void libtrace_scb_destroy(libtrace_scb_t *buf);
+DLLEXPORT int libtrace_scb_recv_sock(libtrace_scb_t *buf, int sock,
+                int recvflags);
+DLLEXPORT uint8_t *libtrace_scb_get_read(libtrace_scb_t *buf,
+                uint32_t *available);
+DLLEXPORT void libtrace_scb_advance_read(libtrace_scb_t *buf, uint32_t forward);
 
 #endif
