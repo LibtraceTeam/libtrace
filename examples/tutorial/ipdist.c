7d86ee27f0ce517872f0a110105c67bd704a6074
Jacob Van Walraven <jcv9@spectre.cms.waikato.ac.nz>
2018-11-07 11:34:25 +1300

    Added some error checking

--- examples/tutorial/ipdist.c
+++ examples/tutorial/ipdist.c
@@ -32,7 +32,10 @@ static void plot_results() {
 
 	/* Commands that need to be sent to gnuplot */
 	char *commands[] = {"set term png size 1280,960",
+			    "set title 'IP Distrubtion'",
 			    "set xrange [0:255]",
+			    "set xlabel 'Prefix'",
+			    "set ylabel 'Hits'",
 			    "set xtics 0,10,255",
 			    "set output 'ipdist.png'",
 			    "plot 'ipdist.tmp' using 1:2 title 'Source Address' with boxes, 'ipdist.tmp' using 1:3 title 'Destination Address' with boxes",
@@ -40,7 +43,7 @@ static void plot_results() {
 	/* Open pipe to gnuplot */
 	FILE *gnuplot = popen("gnuplot -persistent", "w");
 	/* send all commands to gnuplot */
-	for(i=0;i<6;i++) {
+	for(i=0;i<9;i++) {
 		fprintf(gnuplot, "%s \n", commands[i]);
 	}
 	pclose(gnuplot);
@@ -77,8 +80,7 @@ static void process_ip(struct sockaddr *ip, int srcaddr) {
 		/* Ensure the address is in network byte order */
 		uint32_t address = htonl(ip4.s_addr);
 
-		/* Check if the address is part of an excluded network.
-		   This is performed before splitting the into octets so if performed over entire address. */
+		/* Check if the address is part of an excluded network. */
 		if(network_excluded(address) == 0) {
 
 			/* Split the IPv4 address into each octet */
@@ -88,15 +90,14 @@ static void process_ip(struct sockaddr *ip, int srcaddr) {
 			octet[2] = (address & 0x0000ff00) >> 8;
 			octet[3] = (address & 0x000000ff);
 
-			/* check if the supplied address was a source or destination, increment
-			   the correct one */
+			/* check if the supplied address was a source or destination,
+			   increment the correct one */
 			if(srcaddr) {
 				srcaddrcount[octet[0]]++;
 			} else {
 				dstaddrcount[octet[0]]++;
 			}
 		}
-
         }
 }
 
@@ -117,7 +118,16 @@ static void per_packet(libtrace_packet_t *packet) {
 	if(address != NULL) {
 		process_ip(address, 0);
 	}
+}
 
+static void libtrace_cleanup(libtrace_t *trace, libtrace_packet_t *packet) {
+	/* Only destroy trace and packet if they are not NULL */
+	if(trace) {
+		trace_destroy(trace);
+	}
+	if(packet) {
+		trace_destroy_packet(packet);
+	}
 }
 
 int main(int argc, char *argv[]) {
@@ -127,7 +137,8 @@ int main(int argc, char *argv[]) {
 
 	/* Ensure the input URI was supplied */
         if(argc < 2) {
-                fprintf(stderr, "Usage: %s inputURI\n", argv[0]);
+                fprintf(stderr, "Usage: %s inputURI [excluded networks]\n", argv[0]);
+		fprintf(stderr, "       eg. ./ipdist input.erf 210.10.3.0/24 70.5.0.0/16\n");
                 return 1;
         }
 
@@ -148,39 +159,53 @@ int main(int argc, char *argv[]) {
 
 	char delim[] = "/";
 	// Convert supplied address and mask to a exclude_network structure
-	for(i=2;i<argc;i++) {
-		char *address = strtok(argv[i], delim);
+	for(i=0;i<argc-2;i++) {
+		char *address = strtok(argv[i+2], delim);
 		char *mask = strtok(NULL, delim);
 
+		/* Check the subnet mask is valid */
+		if(atoi(mask) == 0 || atoi(mask) > 32 || atoi(mask) < 0) {
+			fprintf(stderr, "Invalid subnet mask: %s\n", mask);
+                        return 1;
+		}
 		/* right shift so netmask is in network byte order */
-        	exclude[i-2].mask = 0xffffffff << (32 - atoi(mask));
+        	exclude[i].mask = 0xffffffff << (32 - atoi(mask));
 
         	struct in_addr addr;
-        	/* Convert address string into uint32_t */
-        	inet_aton(address, &addr);
+        	/* Convert address string into uint32_t and check its valid*/
+        	if(inet_aton(address, &addr) == 0) {
+			fprintf(stderr, "Invalid exclude address: %s\n", address);
+			return 1;
+		}
 		/* Ensure its saved in network byte order */
-        	exclude[i-2].address = htonl(addr.s_addr);
+        	exclude[i].address = htonl(addr.s_addr);
 
 		/* Calculate the network address */
-		exclude[i-2].network = exclude[i-2].address & exclude[i-2].mask;
+		exclude[i].network = exclude[i].address & exclude[i].mask;
 	}
 
 	/* Create the packet structure */
 	packet = trace_create_packet();
+	/* Ensure no error has occured creating the packet */
+	if(packet == NULL) {
+		perror("Creating libtrace packet");
+		libtrace_cleanup(trace, packet);
+		return 1;
+	}
 
 	/* Create the trace */
 	trace = trace_create(argv[1]);
-
 	/* Ensure no error has occured creating the trace */
 	if(trace_is_err(trace)) {
 		trace_perror(trace, "Opening trace file");
+		libtrace_cleanup(trace, packet);
 		return 1;
 	}
 
 	/* Start the trace, if it errors return */
 	if(trace_start(trace) == -1) {
 		trace_perror(trace, "Starting trace");
-		trace_destroy(trace);
+		libtrace_cleanup(trace, packet);
 		return 1;
 	}
 
@@ -193,6 +218,7 @@ int main(int argc, char *argv[]) {
 	   the entire trace */
 	if(trace_is_err(trace)) {
 		trace_perror(trace, packet);
+		libtrace_cleanup(trace, packet);
 		return 1;
 	}
 
@@ -202,8 +228,8 @@ int main(int argc, char *argv[]) {
 	/* Print results */
 	plot_results();
 
-	trace_destroy(trace);
-	trace_destroy_packet(packet);
-	//libtrace_cleanup(trace, packet);
+	/* cleanup */
+	libtrace_cleanup(trace, packet);
+
 	return 0;
 }
