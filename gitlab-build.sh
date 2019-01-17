1c3f8d2d7785836c385f10c23057491fa5fd53cc
Shane Alcock <salcock@waikato.ac.nz>
2019-01-17 13:48:33 +1300

    Reverse attempt to not depend on dpdk-dev for some suites.
    I've decided instead that we *will* provide dpdk 17.11 packages
    for the older suites (within the libtrace repository) so we
    no longer lose any functionality compared with previous libtrace
    packages.

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -11,6 +11,7 @@ apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
     apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git
 
 echo "deb https://dl.bintray.com/wand/general $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/wand.list
+echo "deb https://dl.bintray.com/wand/libtrace $(lsb_release -sc) main" | tee -a /etc/apt/sources.list.d/wand.list
 curl https://packages.wand.net.nz/keyring.gpg -o /etc/apt/trusted.gpg.d/wand.gpg
 
 apt-get update
