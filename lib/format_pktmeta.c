58c226e0e824bed2da102fa859f06bb7dbd1a707
Jacob Van Walraven <jcv9@spectre.cms.waikato.ac.nz>
2019-02-05 15:12:49 +1300

    Move function descriptions to the correct header file, Revert trace_get_interface_ipv4 changes

--- lib/format_pktmeta.c
+++ lib/format_pktmeta.c
@@ -20,33 +20,7 @@ static int trace_meta_check_input(libtrace_packet_t *packet, char *input_func) {
 	return 1;
 }
 
-/* API functions to retrieve interface related packet data */
-
-/* Destroy libtrace_meta_t structure
- *
- * @params libtrace_meta_t structure
- * returns 1 on success, -1 on failure
- */
-int trace_destroy_meta(libtrace_meta_t *result) {
-        int i;
-        if (!result) { return -1; }
-
-	for (i=0;i<result->num;i++) {
-		if(result->items[i].data != NULL) {
-			free(result->items[i].data);
-		}
-	}
-        if (result->items) {
-		free(result->items);
-	}
-	if (result) {
-        	free(result);
-	}
-
-        return 1;
-}
-
-libtrace_meta_t *trace_get_meta_option(libtrace_packet_t *packet, uint32_t section,
+static libtrace_meta_t *trace_get_meta_option(libtrace_packet_t *packet, uint32_t section,
 	uint32_t option) {
 
 	libtrace_meta_t *r = NULL;
@@ -119,13 +93,28 @@ libtrace_meta_t *trace_get_meta_option(libtrace_packet_t *packet, uint32_t secti
 	}
 }
 
-/* Get the interface name/s for a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t meta packet.
- * @returns Pointer to libtrace_meta_t structure containing all found interface names
- * or NULL.
- */
+
+/* API FUNCTIONS */
+
+int trace_destroy_meta(libtrace_meta_t *result) {
+        int i;
+        if (!result) { return -1; }
+
+        for (i=0;i<result->num;i++) {
+                if(result->items[i].data != NULL) {
+                        free(result->items[i].data);
+                }
+        }
+        if (result->items) {
+                free(result->items);
+        }
+        if (result) {
+                free(result);
+        }
+
+        return 1;
+}
+
 libtrace_meta_t *trace_get_interface_name_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_name()")<0) {
 		return NULL;
@@ -143,16 +132,7 @@ libtrace_meta_t *trace_get_interface_name_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the interface name for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the interface name from.
- * @params A pointer to a character buffer to store the interface name in.
- * @params The size of the buffer passed in.
- * @params The interface index within the meta packet.
- * @returns Pointer to the character buffer containing the interface name or NULL.
- */
+
 char *trace_get_interface_name(libtrace_packet_t *packet, char *space, int spacelen,
 	int index) {
 
@@ -176,13 +156,6 @@ char *trace_get_interface_name(libtrace_packet_t *packet, char *space, int space
 	return space;
 }
 
-/* Get the interface MAC address/s for a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t meta packet.
- * @returns Pointer to libtrace_meta_t structure containing all found interface mac
- * addresses or NULL.
- */
 libtrace_meta_t *trace_get_interface_mac_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_mac()")<0) {
                 return NULL;
@@ -199,16 +172,7 @@ libtrace_meta_t *trace_get_interface_mac_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the interface MAC address for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the MAC address from.
- * @params A pointer to a character buffer to store the MAC address in.
- * @params The size of the buffer passed in.
- * @params The interface index within the meta packet.
- * @returns Pointer to the character buffer containing the MAC address or NULL.
- */
+
 char *trace_get_interface_mac(libtrace_packet_t *packet, char *space, int spacelen,
 	int index) {
 
@@ -229,13 +193,6 @@ char *trace_get_interface_mac(libtrace_packet_t *packet, char *space, int spacel
 	return space;
 }
 
-/* Get the interface speed/s from a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t packet.
- * @returns Pointer to libtrace_meta_t structure containing all found interface
- * speeds or NULL.
- */
 libtrace_meta_t *trace_get_interface_speed_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_speed()")<0) {
                 return NULL;
@@ -252,14 +209,7 @@ libtrace_meta_t *trace_get_interface_speed_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the interface speed for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the interface speed from.
- * @params The interface index within the meta packet.
- * @returns uint64_t interface speed or NULL.
- */
+
 uint64_t trace_get_interface_speed(libtrace_packet_t *packet, int index) {
 	libtrace_meta_t *r = trace_get_interface_speed_meta(packet);
 	if (r == NULL) { return 0; }
@@ -274,13 +224,6 @@ uint64_t trace_get_interface_speed(libtrace_packet_t *packet, int index) {
 	return data;
 }
 
-/* Get the interface ipv4 address/s for a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t meta packet.
- * @returns Pointer to libtrace_meta_t structure containing all found ipv4 addresses
- * or NULL
- */
 libtrace_meta_t *trace_get_interface_ipv4_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_ip4()")<0) {
                 return NULL;
@@ -297,14 +240,7 @@ libtrace_meta_t *trace_get_interface_ipv4_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the interface ipv4 address for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the ipv4 address from.
- * @params The interface index within the meta packet.
- * @returns uint32_t ipv4 address or 0.
- */
+
 uint32_t trace_get_interface_ipv4(libtrace_packet_t *packet, int index) {
 	libtrace_meta_t *r = trace_get_interface_ipv4_meta(packet);
 	if (r == NULL) { return 0; }
@@ -316,34 +252,26 @@ uint32_t trace_get_interface_ipv4(libtrace_packet_t *packet, int index) {
 	trace_destroy_meta(r);
 	return data;
 }
-/* Get the interface ipv4 address string for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the ipv4 address from.
- * @params The interface index within the meta packet.
- * @returns Pointer to the character buffer containing the ipv4 address string or NULL.
- */
+
 /* UNTESTED */
-char *trace_get_interface_ipv4_string(libtrace_packet_t *packet, int index) {
-	struct in_addr ip;
-	uint32_t addr;
+char *trace_get_interface_ipv4_string(libtrace_packet_t *packet, char *space, int spacelen,
+	int index) {
 
-	addr = trace_get_interface_ipv4(packet, index);
-	if (addr == 0) { return NULL; }
+	if (spacelen < INET_ADDRSTRLEN) {
+		return NULL;
+	}
 
-	ip.s_addr = addr;
+	uint32_t addr = trace_get_interface_ipv4(packet, index);
+	if (addr == 0) {
+		return NULL;
+	}
 
-	return inet_ntoa(ip);
+	/* get the string representation, store in space */
+	inet_ntop(AF_INET, &addr, space, INET_ADDRSTRLEN);
+
+	return space;
 }
 
-/* Get the interface ipv6 address/s for a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t meta packet.
- * @returns Pointer to libtrace_meta_t structure containing all found ipv6 addresses
- * or NULL.
- */
 libtrace_meta_t *trace_get_interface_ipv6_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_ip6()")<0) {
                 return NULL;
@@ -360,16 +288,7 @@ libtrace_meta_t *trace_get_interface_ipv6_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the interface ipv6 address for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the ipv6 address from.
- * @params A pointer to a character buffer to store the ipv6 address in.
- * @params The size of the buffer passed in.
- * @params The interface index within the meta packet.
- * @returns Pointer to the buffer containing the ipv6 address or NULL.
- */
+
 void *trace_get_interface_ipv6(libtrace_packet_t *packet, void *space, int spacelen,
 	int index) {
 
@@ -387,16 +306,7 @@ void *trace_get_interface_ipv6(libtrace_packet_t *packet, void *space, int space
 	trace_destroy_meta(r);
 	return space;
 }
-/* Get the interface ipv6 address string for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the ipv6 address from.
- * @params A pointer to a character buffer to store the ipv6 address in.
- * @params The size of the buffer passed in.
- * @params The interface index within the meta packet.
- * @returns Pointer to the character buffer containing the ipv6 address string or NULL.
- */
+
 /* UNTESTED */
 char *trace_get_interface_ipv6_string(libtrace_packet_t *packet, char *space, int spacelen,
 	int index) {
@@ -413,20 +323,12 @@ char *trace_get_interface_ipv6_string(libtrace_packet_t *packet, char *space, in
 		return NULL;
 	}
 
-	/* get the string representation */
+	/* get the string representation, store in space */
 	inet_ntop(AF_INET6, &(sa.sin6_addr), space, INET6_ADDRSTRLEN);
 
 	return space;
 }
 
-
-/* Get the interface description/s for a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t meta packet.
- * @returns Pointer to libtrace_meta_t structure containing all found interface
- * descriptions or NULL.
- */
 libtrace_meta_t *trace_get_interface_description_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_description()")<0) {
                 return NULL;
@@ -443,16 +345,7 @@ libtrace_meta_t *trace_get_interface_description_meta(libtrace_packet_t *packet)
 
 	return r;
 }
-/* Get the interface description for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the interface description from.
- * @params A pointer to a character buffer to store the interface description in.
- * @params The size of the buffer passed in.
- * @params The interface index within the meta packet.
- * @returns Pointer to the character buffer containing the interface description or NULL.
- */
+
 char *trace_get_interface_description(libtrace_packet_t *packet, char *space, int spacelen,
 	int index) {
 
@@ -473,13 +366,6 @@ char *trace_get_interface_description(libtrace_packet_t *packet, char *space, in
 	return space;
 }
 
-
-/* Get the host OS for a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t meta packet.
- * @returns Pointer to libtrace_meta_t structure containing the host OS or NULL.
- */
 libtrace_meta_t *trace_get_host_os_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_host_os()")<0) {
                 return NULL;
@@ -496,13 +382,7 @@ libtrace_meta_t *trace_get_host_os_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the host OS for a meta packet.
- *
- * @params libtrace_packet_t meta packet to extract the host OS from.
- * @params A pointer to a character buffer to store the host OS in.
- * @params The size of the buffer passed in.
- * @returns Pointer to the character buffer containing the host OS or NULL.
- */
+
 char *trace_get_host_os(libtrace_packet_t *packet, char *space, int spacelen) {
 	libtrace_meta_t *r = trace_get_host_os_meta(packet);
 	if (r == NULL) { return NULL; }
@@ -517,13 +397,6 @@ char *trace_get_host_os(libtrace_packet_t *packet, char *space, int spacelen) {
 	return space;
 }
 
-/* Get the interface frame check sequence length for a meta packet.
- * Must be destroyed with trace_destroy_meta().
- *
- * @params libtrace_packet_t meta packet.
- * @returns Pointer to libtrace_meta_t structure containing all found frame check
- * sequence lengths or NULL.
- */
 libtrace_meta_t *trace_get_interface_fcslen_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_frame_check_sequence_length()")<0) {
                 return NULL;
@@ -540,14 +413,7 @@ libtrace_meta_t *trace_get_interface_fcslen_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the interface frame check sequence length for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface index.
- *
- * @params libtrace_packet_t meta packet to extract the interface fcslen from.
- * @params The interface index within the meta packet.
- * @returns uint32_t frame check sequence length or 0.
- */
+
 uint32_t trace_get_interface_fcslen(libtrace_packet_t *packet, int index) {
 	libtrace_meta_t *r = trace_get_interface_fcslen_meta(packet);
 	if (r == NULL) { return 0; }
@@ -560,12 +426,6 @@ uint32_t trace_get_interface_fcslen(libtrace_packet_t *packet, int index) {
 	return data;
 }
 
-/* Get any interface comments for a meta packet
- * Must be destroyed with trace_destroy_meta()
- *
- * @params libtrace_packet_t packet
- * @returns Pointer to libtrace_meta_t structure or NULL
- */
 libtrace_meta_t *trace_get_interface_comment_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_comment()")<0) {
                 return NULL;
@@ -582,16 +442,7 @@ libtrace_meta_t *trace_get_interface_comment_meta(libtrace_packet_t *packet) {
 
 	return r;
 }
-/* Get the interface comment for a meta packet.
- * Note: ERF packets can contain multiple interfaces per meta packet. Use index to
- * specify the interface ID.
- *
- * @params libtrace_packet_t meta packet to extract the interface comment from.
- * @params A pointer to a character buffer to store the interface description in.
- * @params The size of the buffer passed in.
- * @params The interface number within the meta packet.
- * @returns Pointer to the character buffer containing the hardware description or NULL.
- */
+
 char *trace_get_interface_comment(libtrace_packet_t *packet, char *space, int spacelen,
 	int index) {
 
@@ -612,12 +463,6 @@ char *trace_get_interface_comment(libtrace_packet_t *packet, char *space, int sp
 	return space;
 }
 
-/* Get the capture application for a meta packet
- * Must be destroyed with trace_destroy_meta()
- *
- * @params libtrace_packet_t packet
- * @returns Pointer to libtrace_meta_t structure or NULL
- */
 libtrace_meta_t *trace_get_capture_application_meta(libtrace_packet_t *packet) {
 	if (trace_meta_check_input(packet, "trace_get_interface_comment()")<0) {
                 return NULL;
@@ -634,13 +479,7 @@ libtrace_meta_t *trace_get_capture_application_meta(libtrace_packet_t *packet) {
 
         return r;
 }
-/* Get the capture application for a meta packet.
- *
- * @params libtrace_packet_t meta packet to extract the application name from.
- * @params A pointer to a character buffer to store the application name in.
- * @params The size of the buffer passed in.
- * @returns Pointer to the character buffer containing the application name or NULL.
- */
+
 char *trace_get_capture_application(libtrace_packet_t *packet, char *space, int spacelen) {
 	libtrace_meta_t *r = trace_get_capture_application_meta(packet);
 	if (r == NULL) { return NULL; }
@@ -655,14 +494,6 @@ char *trace_get_capture_application(libtrace_packet_t *packet, char *space, int 
 	return space;
 }
 
-/* Get a meta section option from a meta packet
- * Must be destroyed with trace_destroy_meta()
- *
- * @params libtrace_packet_t packet
- * @params Section code
- * @params Option code
- * @returns Pointer to libtrace_meta_t structure or NULL
- */
 libtrace_meta_t *trace_get_section_option(libtrace_packet_t *packet, uint32_t section_code,
 	uint16_t option_code) {
 
@@ -673,13 +504,6 @@ libtrace_meta_t *trace_get_section_option(libtrace_packet_t *packet, uint32_t se
 	return trace_get_meta_option(packet, section_code, option_code);
 }
 
-/* Get a section from a meta packet
- * Must be destroyed with trace_destroy_meta()
- *
- * @params libtrace_packet_t packet
- * @params Section code
- * @returns Pointer to libtrace_meta_t structure or NULL
- */
 libtrace_meta_t *trace_get_section(libtrace_packet_t *packet, uint32_t section_code) {
 	if (trace_meta_check_input(packet, "trace_get_section()")<0) {
                 return NULL;
@@ -688,14 +512,6 @@ libtrace_meta_t *trace_get_section(libtrace_packet_t *packet, uint32_t section_c
 	return packet->trace->format->get_meta_section(packet, section_code);
 }
 
-/* ERF specific function */
-/* Get the DAG card model from a meta packet.
- *
- * @params libtrace_packet_t meta packet to extract the DAG model from.
- * @params A pointer to a character buffer to store the DAG model in.
- * @params The size of the buffer passed in.
- * @returns Pointer to the character buffer containing the DAG model or NULL.
- */
 char *trace_get_erf_dag_card_model(libtrace_packet_t *packet, char *space, int spacelen) {
 	libtrace_meta_t *r = trace_get_section_option(packet, ERF_PROV_SECTION_MODULE,
 		ERF_PROV_MODEL);
@@ -711,13 +527,7 @@ char *trace_get_erf_dag_card_model(libtrace_packet_t *packet, char *space, int s
 	trace_destroy_meta(r);
 	return space;
 }
-/* Get the host DAG software version for a meta packet.
- *
- * @params libtrace_packet_t meta packet to extract the hosts DAG verion from.
- * @params A pointer to a character buffer to store the DAG version in.
- * @params The size of the buffer passed in.
- * @returns Pointer to the character buffer containing the DAG version or NULL.
- */
+
 char *trace_get_erf_dag_version(libtrace_packet_t *packet, char *space, int spacelen) {
 	libtrace_meta_t *r = trace_get_section_option(packet, ERF_PROV_SECTION_MODULE,
 		ERF_PROV_DAG_VERSION);
@@ -734,13 +544,7 @@ char *trace_get_erf_dag_version(libtrace_packet_t *packet, char *space, int spac
 	trace_destroy_meta(r);
 	return space;
 }
-/* Get the firmware version for a DAG module from a meta packet.
- *
- * @params libtrace_packet_t meta packet to extract the FW version from.
- * @params A pointer to a character buffer to store the FW version in.
- * @params The size of the buffer passed in.
- * @returns Pointer to the character buffer containing the FW version or NULL.
- */
+
 char *trace_get_erf_dag_fw_version(libtrace_packet_t *packet, char *space, int spacelen) {
 	libtrace_meta_t *r = trace_get_section_option(packet, ERF_PROV_SECTION_MODULE,
 		ERF_PROV_FW_VERSION);
