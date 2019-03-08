115acdbe80fbfbfd78a270cd57f4207f307c3947
Shane Alcock <salcock@waikato.ac.nz>
2019-03-08 16:43:50 +1300

    Update bintray-upload.sh to Brad's latest version

--- bintray-upload.sh
+++ bintray-upload.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
-set -x -e -o pipefail
+set -e -o pipefail
 
-VERSION=${CI_COMMIT_REF_NAME}
-PKGVERSION=1
-ARCH=amd64
+BINTRAY_REPO="wand/libtrace"
+BINTRAY_LICENSE="LGPL-3.0"
 
-APIKEY=${BINTRAY_API_KEY}
+apt-get update && apt-get install -y curl
 
-apt-get update
-apt-get install -y curl
+curl --silent -fL -XGET \
+    "https://api.bintray.com/content/jfrog/jfrog-cli-go/\$latest/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64" \
+    > /usr/local/bin/jfrog
+chmod +x /usr/local/bin/jfrog
+mkdir ~/.jfrog/
+cat << EOF > ~/.jfrog/jfrog-cli.conf
+{
+  "artifactory": null,
+  "bintray": {
+    "user": "${BINTRAY_USERNAME}",
+    "key": "${BINTRAY_API_KEY}"
+  },
+  "Version": "1"
+}
+EOF
 
-UBUNTU_DISTS=("xenial" "artful" "bionic")
-DEBIAN_DISTS=("stretch" "jessie" "sid" "buster")
-PACKAGE_LIST=("libtrace4" "libtrace4-dev" "libtrace4-tools" "libpacketdump4" "libpacketdump4-dev")
-
-for i in "${UBUNTU_DISTS[@]}"
-do
-        for comp in "${PACKAGE_LIST[@]}"; do
-                echo ubuntu_$i/${comp}
-
-                if [ ! -f built-packages/ubuntu_$i/${comp}_${VERSION}-${PKGVERSION}_${ARCH}.deb ]; then
-                        continue
-                fi
-
-                curl -T built-packages/ubuntu_$i/${comp}_${VERSION}-${PKGVERSION}_${ARCH}.deb -usalcock:$APIKEY "https://api.bintray.com/content/wand/libtrace/$comp/$VERSION/pool/$i/main/${comp}_${VERSION}-${PKGVERSION}_${ARCH}.deb;deb_distribution=$i;deb_component=main;deb_architecture=$ARCH"
-        done
+for path in `find built-packages/ -maxdepth 1 -type d`; do
+    IFS=_ read linux_version <<< $(basename "${path}")
+    for deb in `find "${path}" -maxdepth 1 -type f`; do
+        pkg_filename=$(basename "${deb}")
+        IFS=_ read pkg_name pkg_version pkg_arch <<< $(basename -s ".deb" "${pkg_filename}")
+        jfrog bt package-create --licenses ${BINTRAY_LICENSE} --vcs-url ${CI_PROJECT_URL} ${BINTRAY_REPO}/${pkg_name} || true
+        jfrog bt upload --deb ${linux_version}/main/${pkg_arch} ${deb} ${BINTRAY_REPO}/${pkg_name}/${pkg_version} pool/${linux_version}/main/${pkg_name}/
+    done
 done
 
-for i in "${DEBIAN_DISTS[@]}"
-do
-        for comp in "${PACKAGE_LIST[@]}"; do
-                echo debian_$i/${comp}
-                if [ ! -f built-packages/debian_$i/${comp}_${VERSION}-${PKGVERSION}_${ARCH}.deb ]; then
-                        continue
-                fi
-
-                curl -T built-packages/debian_$i/${comp}_${VERSION}-${PKGVERSION}_${ARCH}.deb -usalcock:$APIKEY "https://api.bintray.com/content/wand/libtrace/$comp/$VERSION/pool/$i/main/${comp}_${VERSION}-${PKGVERSION}_${ARCH}.deb;deb_distribution=$i;deb_component=main;deb_architecture=$ARCH"
-        done
-done
