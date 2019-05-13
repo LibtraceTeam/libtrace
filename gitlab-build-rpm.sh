34e5f34abd56917e4193bb540c6d17bb0f16507c
Shane Alcock <salcock@waikato.ac.nz>
2019-05-13 16:40:04 +1200

    Update gitlab package building scripts to build RPMs correctly
     * Add *correct* wand RPM repos to /etc/yum.repos.d/
     * Correctly strip post-dash portions of the git tag to ensure
       that the source file tarball name matches what the rpm spec
       file is expecting.
     * Don't fail if epel packages are already installed on Centos.
     * Make sure we install 'which' on Fedora so that the bootstrap.sh
       script can run properly.

--- gitlab-build-rpm.sh
+++ gitlab-build-rpm.sh
@@ -1,23 +1,54 @@
 set -x -e -o pipefail
 
 export QA_RPATHS=$[ 0x0001 ]
+SOURCENAME=`echo ${CI_COMMIT_REF_NAME} | cut -d '-' -f 1`
 
-yum install -y wget
 
-wget https://bintray.com/wand/general-rpm/rpm -O bintray-wand-general-rpm.repo
-mv bintray-wand-general-rpm.repo /etc/yum.repos.d/
-
+DISTRO=fedora
 if [ "$1" = "centos7" ]; then
-        yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+        DISTRO=centos
 fi
 
 if [ "$1" = "centos6" ]; then
-        yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+        DISTRO=centos
+fi
+
+cat << EOF > /etc/yum.repos.d/bintray-wand-general-rpm.repo
+#bintray-wand-general-rpm - packages by wand from Bintray
+[bintray-wand-general-rpm]
+name=bintray-wand-general-rpm
+baseurl=https://dl.bintray.com/wand/general-rpm/${DISTRO}/\$releasever/\$basearch/
+gpgkey=https://bintray.com/user/downloadSubjectPublicKey?username=wand
+gpgcheck=0
+repo_gpgcheck=1
+enabled=1
+EOF
+
+cat << EOF > /etc/yum.repos.d/bintray-wand-libtrace-rpm.repo
+#bintray-wand-libtrace-rpm - packages by wand from Bintray
+[bintray-wand-libtrace-rpm]
+name=bintray-wand-libtrace-rpm
+baseurl=https://dl.bintray.com/wand/libtrace-rpm/${DISTRO}/\$releasever/\$basearch/
+gpgkey=https://bintray.com/user/downloadSubjectPublicKey?username=wand
+gpgcheck=0
+repo_gpgcheck=1
+enabled=1
+EOF
+
+yum install -y wget make gcc
+
+if [ "$1" = "centos7" ]; then
+        yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm || true
+fi
+
+if [ "$1" = "centos6" ]; then
+        yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm || true
         yum install -y epel-rpm-macros
 fi
 
+
 if [[ "$1" =~ fedora* ]]; then
-        dnf install -y rpm-build rpmdevtools 'dnf-command(builddep)'
+        dnf install -y rpm-build rpmdevtools 'dnf-command(builddep)' which
         dnf group install -y "C Development Tools and Libraries"
         dnf builddep -y rpm/libtrace4.spec
 else
@@ -29,7 +60,7 @@ fi
 rpmdev-setuptree
 
 ./bootstrap.sh && ./configure && make dist
-cp libtrace-*.tar.gz ~/rpmbuild/SOURCES/${CI_COMMIT_REF_NAME}.tar.gz
+cp libtrace-*.tar.gz ~/rpmbuild/SOURCES/${SOURCENAME}.tar.gz
 cp rpm/libtrace4.spec ~/rpmbuild/SPECS/
 
 cd ~/rpmbuild && rpmbuild -bb --define "debug_package %{nil}" SPECS/libtrace4.spec
