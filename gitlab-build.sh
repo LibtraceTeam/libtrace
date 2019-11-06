5b9768729bce41b0fffa761e9c3a06d1124099ce
Shane Alcock <salcock@waikato.ac.nz>
2019-11-06 15:19:12 +1300

    deb CI: only add libtrace repo if building for a distro that needs our dpdk

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -13,10 +13,19 @@ apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
     apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git \
     pkg-config
 
-echo "deb https://dl.bintray.com/wand/general $(lsb_release -sc) main" | \
+DISTRO=$(lsb_release -sc)
+
+echo "deb https://dl.bintray.com/wand/general ${DISTRO} main" | \
     tee -a /etc/apt/sources.list.d/wand.list
-echo "deb https://dl.bintray.com/wand/libtrace $(lsb_release -sc) main" | \
-    tee -a /etc/apt/sources.list.d/wand.list
+
+
+case ${DISTRO} in
+        jessie | xenial | stretch )
+                echo "deb https://dl.bintray.com/wand/libtrace ${DISTRO} main" \
+                        | tee -a /etc/apt/sources.list.d/wand.list
+        ;;
+esac
+
 curl --silent "https://bintray.com/user/downloadSubjectPublicKey?username=wand"\
     | apt-key add -
 
