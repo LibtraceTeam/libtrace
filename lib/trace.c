d93bfca65f3cec656cc5dc1ae6620380853a3b3a
Shane Alcock <salcock@waikato.ac.nz>
2020-03-24 15:36:44 -0700

    Fix bad "remaining" value for corsaro-tagged packets when wire length was small
    Capture length vs wire length considerations (i.e. choosing the
    smaller of the two) will now only happen at layer 2 onwards, not
    at the meta-data layer.
    
    This is because meta-data is often added to a captured packet,
    and therefore will extend the capture length but leave the wire
    length as it was (the meta data was never "on" the wire).
    
    Therefore, trying to correct for packets with extra capture
    padding at the meta layer is incorrect, and was causing issues
    where corsaro-tagged packets were appearing to have
    truncated content because the value we were returning as
    "remaining" had been adjusted to match the smaller wire length.

--- lib/trace.c
+++ lib/trace.c
@@ -1190,8 +1190,6 @@ DLLEXPORT int trace_write_packet(libtrace_out_t *libtrace, libtrace_packet_t *pa
 /* Get a pointer to the first byte of the packet payload */
 DLLEXPORT void *trace_get_packet_buffer(const libtrace_packet_t *packet,
 		libtrace_linktype_t *linktype, uint32_t *remaining) {
-	int cap_len;
-	int wire_len;
         libtrace_linktype_t ltype;
 
 	if (!packet) {
@@ -1212,42 +1210,7 @@ DLLEXPORT void *trace_get_packet_buffer(const libtrace_packet_t *packet,
         }
 
 	if (remaining) {
-		/* I think we should choose the minimum of the capture and
-		 * wire lengths to be the "remaining" value. If the packet has
-		 * been padded to increase the capture length, we don't want
-		 * to allow subsequent protocol decoders to consider the
-		 * padding as part of the packet.
-		 *
-		 * For example, in Auck 4 there is a trace where the IP header
-		 * length is incorrect (24 bytes) followed by a 20 byte TCP
-		 * header. Total IP length is 40 bytes. As a result, the
-		 * legacyatm padding gets treated as the "missing" bytes of
-		 * the TCP header, which isn't the greatest. We're probably
-		 * better off returning an incomplete TCP header in that case.
-		 */
-
-		cap_len = trace_get_capture_length(packet);
-		wire_len = trace_get_wire_length(packet);
-
-		if (!(cap_len >= 0)) {
-			fprintf(stderr, "Was expecting capture length of atleast 0 in trace_get_packet_buffer()\n");
-			return NULL;
-		}
-
-		/* There is the odd corrupt packet, e.g. in IPLS II, that have
-		 * massively negative wire lens. We could assert fail here on
-		 * them, but we could at least try the capture length instead.
-		 *
-		 * You may still run into problems if you try to write that
-		 * packet, but at least reading should work OK.
-		 */
-		if (wire_len < 0)
-			*remaining = cap_len;
-		else if (wire_len < cap_len)
-			*remaining = wire_len;
-		else
-			*remaining = cap_len;
-		/* *remaining = trace_get_capture_length(packet); */
+		*remaining = trace_get_capture_length(packet);
 	}
 	return (void *) packet->payload;
 }
