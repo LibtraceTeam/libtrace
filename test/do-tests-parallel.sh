82848c21efa394bc3387226f3c4c2e34cbeead2f
Jacob Van Walraven <jacob.vanwalraven@waikato.ac.nz>
2021-03-22 14:45:47 +1300

    Fix args

--- test/do-tests-parallel.sh
+++ test/do-tests-parallel.sh
@@ -18,46 +18,46 @@ export DYLD_LIBRARY_PATH="${libdir}"
 
 rm -f traces/*.out.*
 echo \* Read erf
-do_test ./test-format-parallel erf
+do_test ./test-format-parallel -r erf
 
 echo \* Read erf provenance
-do_test ./test-format-parallel erfprov
+do_test ./test-format-parallel -r erfprov
 
 echo \* Read pcap
-do_test ./test-format-parallel pcap
+do_test ./test-format-parallel -r pcap
 
 echo \* Read pcapfile
-do_test ./test-format-parallel pcapfile
+do_test ./test-format-parallel -r pcapfile
 
 echo \* Read pcapfilens
-do_test ./test-format-parallel pcapfilens
+do_test ./test-format-parallel -r pcapfilens
 
 echo \* Read legacyatm
-do_test ./test-format-parallel legacyatm
+do_test ./test-format-parallel -r legacyatm
 
 echo \* Read legacyeth
-do_test ./test-format-parallel legacyeth
+do_test ./test-format-parallel -r legacyeth
 
 echo \* Read legacypos
-do_test ./test-format-parallel legacypos
+do_test ./test-format-parallel -r legacypos
 
 echo \* Read tsh
-do_test ./test-format-parallel tsh
+do_test ./test-format-parallel -r tsh
 
 echo \* Read rawerf
-do_test ./test-format-parallel rawerf 
+do_test ./test-format-parallel -r rawerf 
 
 echo \* Read pcapng
-do_test ./test-format-parallel pcapng
+do_test ./test-format-parallel -r pcapng
 
 echo \* Read testing hasher function
-do_test ./test-format-parallel-hasher erf
+do_test ./test-format-parallel-hasher -r erf
 
 echo \* Read testing single-threaded datapath
-do_test ./test-format-parallel-singlethreaded erf
+do_test ./test-format-parallel-singlethreaded -r erf
 
 echo \* Read testing single-threaded hasher datapath
-do_test ./test-format-parallel-singlethreaded-hasher erf
+do_test ./test-format-parallel-singlethreaded-hasher -r erf
 
 echo \* Read stress testing with 100 threads
 do_test ./test-format-parallel-stressthreads erf
