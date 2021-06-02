c450c6235bc5bd1b668247abf9f6165fa11711db
Richard Sanger <rsanger@wand.net.nz>
2021-06-02 13:51:15 +1200

    etsilive: Replace hard coded recv threshold
    Replaces the hard coded recv threshold from 60f5a69
    with an adjustable threshold. This threshold defaults to 10%.

--- lib/data-struct/simple_circular_buffer.h
+++ lib/data-struct/simple_circular_buffer.h
@@ -10,6 +10,7 @@ typedef struct libtracescb {
         uint32_t write_offset;
         uint32_t read_offset;
         int fd;
+        uint32_t recv_thold;
 } libtrace_scb_t;
 
 
@@ -23,4 +24,6 @@ DLLEXPORT uint8_t *libtrace_scb_get_read(libtrace_scb_t *buf,
 DLLEXPORT void libtrace_scb_advance_read(libtrace_scb_t *buf, uint32_t forward);
 DLLEXPORT int libtrace_scb_get_available_space(libtrace_scb_t *buf);
 DLLEXPORT int libtrace_scb_get_size(libtrace_scb_t *buf);
+DLLEXPORT int libtrace_scb_set_recv_thold(libtrace_scb_t *buf, uint32_t thold);
+DLLEXPORT uint32_t libtrace_scb_get_recv_thold(libtrace_scb_t *buf);
 #endif
