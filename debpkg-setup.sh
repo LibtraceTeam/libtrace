649b0851365ba1fcb0fc6208f82a0816afc63af2
Shane Alcock <salcock@waikato.ac.nz>
2021-07-20 10:27:21 +1200

    Fix debhelper issues for xenial / stretch / bionic
    These distros don't have debhelper 12 by default, so we either
    need to install a backport or downgrade our dependency to
    accommodate what is available.

--- debpkg-setup.sh
+++ debpkg-setup.sh
@@ -11,7 +11,7 @@ export SOURCENAME=`echo ${GITHUB_REF##*/} | cut -d '-' -f 1`
 apt-get update
 apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
     apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git \
-    pkg-config
+    pkg-config sed
 
 curl -1sLf 'https://dl.cloudsmith.io/public/wand/libwandio/cfg/setup/bash.deb.sh' | bash
 curl -1sLf 'https://dl.cloudsmith.io/public/wand/libwandder/cfg/setup/bash.deb.sh' | bash
@@ -19,8 +19,21 @@ curl -1sLf 'https://dl.cloudsmith.io/public/wand/libwandder/cfg/setup/bash.deb.s
 DISTRO=$(lsb_release -sc)
 
 case ${DISTRO} in
-        jessie | xenial | stretch )
+        xenial )
                 curl -1sLf 'https://dl.cloudsmith.io/public/wand/dpdk-wand/cfg/setup/bash.deb.sh' | bash
+                apt-get install -y debhelper -t xenial-backports
+                sed -i 's/debhelper-compat (= 12)/debhelper (>= 10)/' debian/control
+                echo "10" > debian/compat
+        ;;
+
+        stretch )
+                curl -1sLf 'https://dl.cloudsmith.io/public/wand/dpdk-wand/cfg/setup/bash.deb.sh' | bash
+                sed -i 's/debhelper-compat (= 12)/debhelper (>= 10)/' debian/control
+                echo "10" > debian/compat
+        ;;
+
+        bionic )
+                apt-get install -y debhelper -t bionic-backports
         ;;
 esac
 
